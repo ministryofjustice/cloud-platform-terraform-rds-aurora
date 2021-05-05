928e5cf30db380cee6e5e2c488a6f875b5fd4d79
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-05-05 14:25:11 +0100

    Add vpc with default cluster name

--- test/unit-tests/main.tf
+++ test/unit-tests/main.tf
@@ -15,11 +15,55 @@ provider "aws" {
   }
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+
+###########################
+# Locals & Data Resources #
+###########################
+
+locals {
+  vpc_name             = "localstack-terratest"
+  vpc_base_domain_name = "${local.vpc_name}.cloud-platform.service.justice.gov.uk"
+  cluster_tags = {
+    for name in var.cluster_names :
+    "kubernetes.io/cluster/${name}" => "shared"
+  }
+  vpc_tags = merge({
+    "kubernetes.io/cluster/${local.vpc_name}" = "shared"
+  }, local.cluster_tags)
+}
+
+#######
+# VPC #
+#######
+
+module "vpc" {
+  version = "2.18.0"
+  source  = "terraform-aws-modules/vpc/aws"
+
+  name                 = local.vpc_name
+  cidr                 = var.vpc_cidr
+  azs                  = var.availability_zones
+  private_subnets      = var.internal_subnets
+  public_subnets       = var.external_subnets
+  enable_nat_gateway   = true
+  enable_vpn_gateway   = false
+  enable_dns_hostnames = true
+
+  public_subnet_tags = merge({
+    SubnetType               = "Utility"
+    "kubernetes.io/role/elb" = "1"
+  }, local.cluster_tags)
+
+  private_subnet_tags = merge({
+    SubnetType                        = "Private"
+    "kubernetes.io/role/internal-elb" = "1"
+  }, local.cluster_tags)
+
+  vpc_tags = local.vpc_tags
 
   tags = {
-    Name = "localstack-terratest"
+    Terraform = "true"
+    Cluster   = local.vpc_name
+    Domain    = local.vpc_base_domain_name
   }
-
 }
