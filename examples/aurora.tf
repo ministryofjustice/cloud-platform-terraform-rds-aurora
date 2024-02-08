55e6fa9ed74f70eec02f1f55eca5789d0c88310a
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-02-08 17:09:14 +0000

    update examples

--- examples/aurora.tf
+++ examples/aurora.tf
@@ -1,6 +1,7 @@
 module "rds_aurora" {
-  source = "../"
-
+  source = "../" # use latest release
+  # source = "github.com/ministryofjustice/cloud-platform-terraform-rds-aurora?ref=version" # use latest release
+  
   # VPC configuration
   vpc_name = var.vpc_name
 
